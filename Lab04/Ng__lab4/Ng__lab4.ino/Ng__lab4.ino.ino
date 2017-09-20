7cfe8d43197d93b837d6e7b373bd9edc93898ff5
Ksnsong <ksn1010@plymouth.edu>
2017-09-20 16:27:48 -0400

    Add files via upload

---
+++ Lab04/Ng__lab4/Ng__lab4.ino/Ng__lab4.ino.ino
@@ -0,0 +1,342 @@
+#include <TimedAction.h>
+//shift register0
+int datapin = 2;          
+int clockpin = 3;
+int latchpin = 4;
+
+//shift register1
+int datapin1 = A5;          
+int clockpin1 = A4;
+int latchpin1 = A3;
+
+
+byte data = 0;
+byte data1 = 0;
+
+//buttons
+int buttonGreen = 9;
+int buttonYellow = 10;
+int buttonWhite = 11;
+
+//tilt sensors             
+const int sensorR=8;
+const int sensorB=6;
+const int sensorF=5;
+const int sensorL=7;
+
+
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;
+
+int strobeBlinkCycle = 0;
+int beaconBlinkCycle = 0;
+
+boolean beaconLights_turnedOn = false;
+boolean strobeLights_turnedOn = false;
+
+//for tilt sensors LEDs
+int ledState = HIGH;                     
+int lastButtonStateL = LOW;
+int lastButtonStateR = LOW;
+int lastButtonStateF = LOW;
+int lastButtonStateB = LOW;      
+
+//Green button
+int ledStateGreen = HIGH; 
+int buttonStateGreen;
+int lastButtonStateGreen = LOW;
+
+//Yellow button
+int ledStateYellow = HIGH; 
+int buttonStateYellow;
+int lastButtonStateYellow = LOW;
+
+//White button
+int ledStateWhite = HIGH; 
+int buttonStateWhite;
+int lastButtonStateWhite = LOW;
+
+//testing
+boolean ledStateG = false;
+boolean ledStateY = false;
+boolean ledStateW = false;
+
+//testing
+void blinkG(){
+  ledStateG ? ledStateG=false : ledStateG=true;
+  shiftWrite1(1, ledStateG);
+  shiftWrite1(2, ledStateG);
+}
+
+//testing
+void blinkW(){
+  ledStateW ? ledStateW=false : ledStateW=true;
+  shiftWrite1(4, ledStateW);
+  shiftWrite1(5, ledStateW);
+  shiftWrite1(6, ledStateW);
+}
+
+TimedAction timedActionG = TimedAction(50,blinkBeacons);
+TimedAction timedActionW = TimedAction(50,blinkStrobes);
+
+void setup() {  
+  Serial.begin(9600);
+  pinMode (sensorR, INPUT);
+  pinMode (sensorB, INPUT);
+  pinMode (sensorF, INPUT);
+  pinMode (sensorL, INPUT);  
+  digitalWrite (sensorR, HIGH);
+  digitalWrite (sensorB, HIGH);
+  digitalWrite (sensorF, HIGH);
+  digitalWrite (sensorL, HIGH);
+  //shift register0
+  pinMode(datapin, OUTPUT);
+  pinMode(clockpin, OUTPUT);  
+  pinMode(latchpin, OUTPUT); 
+  //shift register1
+  pinMode(datapin1, OUTPUT);
+  pinMode(clockpin1, OUTPUT);  
+  pinMode(latchpin1, OUTPUT);
+  //buttons
+  pinMode(buttonGreen, INPUT);
+  pinMode(buttonYellow, INPUT);
+  pinMode(buttonWhite, INPUT);
+  //digitalWrite (buttonGreen, HIGH);
+  shiftWrite1 (1, ledStateGreen);
+  shiftWrite1 (2, ledStateGreen);
+  }
+
+
+ void loop() {  
+  //reading tilt sensors 
+  Serial.println(digitalRead(buttonGreen));
+  int readingR = digitalRead(sensorR);
+  int readingL = digitalRead(sensorL);
+  int readingF = digitalRead(sensorF);
+  int readingB = digitalRead(sensorB);
+  //reading buttons 
+  int readingGreen = digitalRead(buttonGreen);
+  int readingYellow = digitalRead(buttonYellow);
+  int readingWhite = digitalRead(buttonWhite);  
+
+  //Green button
+  if (readingGreen != lastButtonStateGreen) {
+  lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (readingGreen != buttonStateGreen) {
+      buttonStateGreen = readingGreen;
+      if (buttonStateGreen == HIGH) {
+        ledStateGreen = !ledStateGreen;
+        beaconLights_turnedOn=true;
+      }
+    }
+  }
+  if(ledStateGreen==HIGH){
+    timedActionG.check();
+  }else{
+    shiftWrite1(1, ledStateGreen);
+    shiftWrite1(2, ledStateGreen);
+    beaconLights_turnedOn=false;
+  }
+  
+  lastButtonStateGreen = readingGreen;
+
+   //Yellow button
+  if (readingYellow != lastButtonStateYellow) {
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (readingYellow != buttonStateYellow) {
+      buttonStateYellow = readingYellow;
+      if (buttonStateYellow == HIGH) {
+        ledStateYellow = !ledStateYellow;
+      }
+    }
+  }
+  shiftWrite(4, ledStateYellow);
+  shiftWrite1(3, ledStateYellow);
+  lastButtonStateYellow = readingYellow;
+
+   //White button 
+  if (readingWhite != lastButtonStateWhite) {
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (readingWhite != buttonStateWhite) {
+      buttonStateWhite = readingWhite;
+      if (buttonStateWhite == HIGH) {
+        ledStateWhite = !ledStateWhite;
+        strobeLights_turnedOn=true;
+      }
+    }
+  }
+   if(ledStateWhite==HIGH){
+    timedActionW.check();
+  }else{
+    shiftWrite1(4, ledStateWhite);
+    shiftWrite1(5, ledStateWhite);
+    shiftWrite1(6, ledStateWhite);
+    strobeLights_turnedOn=false;
+  }
+  lastButtonStateWhite = readingWhite;
+  
+  //Right
+  if (readingR != lastButtonStateR) {
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (readingR == HIGH) {
+      shiftWrite(0, ledState);
+    }else{
+      ledState=!ledState;
+      shiftWrite(0, ledState);
+      ledState=!ledState;
+    }
+  }
+  lastButtonStateR = readingR;
+
+  //Left
+  if (readingL != lastButtonStateL) {
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (readingL == HIGH) {
+      shiftWrite(2, ledState);
+    }else{
+      ledState=!ledState;
+      shiftWrite(2, ledState);
+      ledState=!ledState;
+    }
+  }
+  lastButtonStateL = readingL;
+
+  //Front
+  if (readingF != lastButtonStateF) {
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (readingF == HIGH) {
+      shiftWrite(1, ledState);
+    }else{
+      ledState=!ledState;
+      shiftWrite(1, ledState);
+      ledState=!ledState;
+    }
+  }
+  lastButtonStateF = readingF;
+
+  //Back
+  if (readingB != lastButtonStateB) {
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (readingB == HIGH) {
+      shiftWrite(3, ledState);
+    }else{
+      ledState=!ledState;
+      shiftWrite(3, ledState);
+      ledState=!ledState;
+    }
+  }
+  lastButtonStateB = readingB;
+}
+
+//shift register0
+void shiftWrite(int desiredPin, boolean desiredState){
+  bitWrite(data,desiredPin,desiredState);
+  shiftOut(datapin, clockpin, MSBFIRST, data);
+  digitalWrite(latchpin, HIGH);
+  digitalWrite(latchpin, LOW);
+}
+//shift register1
+void shiftWrite1(int desiredPin1, boolean desiredState1){
+  bitWrite(data1,desiredPin1,desiredState1);
+  shiftOut(datapin1, clockpin1, MSBFIRST, data1);
+  digitalWrite(latchpin1, HIGH);
+  digitalWrite(latchpin1, LOW);
+}
+
+void blinkStrobes(){
+ if (strobeLights_turnedOn == true) {
+   // Strobe blink pattern courtesy of youtube user BRADHblackdragon in video here:
+   // http://www.youtube.com/watch?v=nnGQ9kB8JG4  at the 3 minute mark
+   // Blink is on at 0 and 2 tenths, off at 1 tenth and 3 through 9 tenths
+   switch (strobeBlinkCycle) {
+     case 0:
+        shiftWrite1(4, HIGH);
+        shiftWrite1(5, HIGH);
+        shiftWrite1(6, HIGH);
+        strobeBlinkCycle ++;
+       break;
+     case 1:
+        shiftWrite1(4, LOW);
+        shiftWrite1(5, LOW);
+        shiftWrite1(6, LOW);
+       strobeBlinkCycle ++;
+       break;
+     case 2:
+        shiftWrite1(4, HIGH);
+        shiftWrite1(5, HIGH);
+        shiftWrite1(6, HIGH);
+       strobeBlinkCycle ++;
+       break;
+     default:
+        shiftWrite1(4, LOW);
+        shiftWrite1(5, LOW);
+        shiftWrite1(6, LOW);
+       strobeBlinkCycle ++;
+       if (strobeBlinkCycle == 10){
+         strobeBlinkCycle = 0;
+       }
+   }
+ }
+}
+
+void blinkBeacons(){
+ if (beaconLights_turnedOn == true) {
+   // Beacon blink pattern courtesy of youtube user BRADHblackdragon in video here:
+   // http://www.youtube.com/watch?v=nnGQ9kB8JG4  at the 3 minute mark
+   switch (beaconBlinkCycle) {
+     case 0:
+       shiftWrite1(1,LOW);
+       shiftWrite1(2,LOW);
+       beaconBlinkCycle ++;
+       break;
+     case 1:
+       shiftWrite1(1,LOW);
+       shiftWrite1(2,LOW);
+       beaconBlinkCycle ++;
+       break;
+     case 2:
+       shiftWrite1(1,LOW);
+       shiftWrite1(2,LOW);
+       beaconBlinkCycle ++;
+       break;
+     case 3:
+       shiftWrite1(1,LOW);
+       shiftWrite1(2,LOW);
+       beaconBlinkCycle ++;
+       break;
+     case 4:
+       shiftWrite1(1,LOW);
+       shiftWrite1(2,LOW);
+       beaconBlinkCycle ++;
+       break;
+     case 5:
+       shiftWrite1(1,HIGH);
+       shiftWrite1(2,HIGH);
+       beaconBlinkCycle ++;
+       break;
+     default:
+       shiftWrite1(1,LOW);
+       shiftWrite1(2,LOW);
+       beaconBlinkCycle ++;
+       if (beaconBlinkCycle == 10){
+         beaconBlinkCycle = 0;
+       }
+   }
+ }
+}
+
+
---
+++ Lab05/Ng_Lab5.ino
@@ -0,0 +1,218 @@
+#include <TimedAction.h>
+#include <Servo.h>
+bool switchFront,switchBack,switchLeft,switchRight;
+bool switchFrontLast=false;
+bool switchBackLast=false;
+bool switchLeftLast=false;
+bool switchRightLast=false;
+unsigned long timeFront,timeBack,timeLeft,timeRight;
+bool button1,button2,button3,button4;
+bool button1Last=false;
+bool button2Last=false;
+bool button3Last=false;
+bool button4Last=false;
+bool flapsUp=false;
+const int flapsUpAngle = 90;
+const int flapsDownAngle = 30;
+boolean beaconLights_turnedOn = false;
+boolean strobeLights_turnedOn = false;
+bool light3=false;
+int strobeBlinkCycle = 0;
+int beaconBlinkCycle = 0;
+const int tiltFrontPin = 0;
+const int tiltBackPin = 1;
+const int tiltLeftPin = 2;
+const int tiltRightPin = 3;
+const int beaconLights_LEDpin = 4;
+const int strobeLights_LEDpin = 5;
+const int landingLightsPin = 6;
+const int button1Pin = 7;
+const int button2Pin = 8;
+const int button3Pin = 12;
+const int button4Pin = 13;
+const int motorPin = 11;
+const int servo1Pin = 10;
+const int servo2Pin = 9;
+const int tiltFrontAnalogPin = 0;
+const int tiltBackAnalogPin = 1;
+const int tiltLeftAnalogPin = 2;
+const int tiltRightAnalogPin = 3;
+const int motorDialAnalogPin = 4;
+Servo servo1;
+Servo servo2;
+
+
+
+void blinkStrobes()
+{
+ if (strobeLights_turnedOn == true) {
+   // Strobe blink pattern courtesy of youtube user BRADHblackdragon in video here:
+   // http://www.youtube.com/watch?v=nnGQ9kB8JG4  at the 3 minute mark
+   // Blink is on at 0 and 2 tenths, off at 1 tenth and 3 through 9 tenths
+   switch (strobeBlinkCycle) {
+     case 0:
+       digitalWrite(strobeLights_LEDpin,HIGH);
+       strobeBlinkCycle ++;
+       break;
+     case 1:
+       digitalWrite(strobeLights_LEDpin,LOW);
+       strobeBlinkCycle ++;
+       break;
+     case 2:
+       digitalWrite(strobeLights_LEDpin,HIGH);
+       strobeBlinkCycle ++;
+       break;
+     default:
+       digitalWrite(strobeLights_LEDpin,LOW);
+       strobeBlinkCycle ++;
+       if (strobeBlinkCycle == 10){
+         strobeBlinkCycle = 0;
+       }
+   }
+ }
+}
+
+void blinkBeacons()
+{
+ if (beaconLights_turnedOn == true) {
+   // Beacon blink pattern courtesy of youtube user BRADHblackdragon in video here:
+   // http://www.youtube.com/watch?v=nnGQ9kB8JG4  at the 3 minute mark
+   switch (beaconBlinkCycle) {
+     case 0:
+       digitalWrite(beaconLights_LEDpin,LOW);
+       beaconBlinkCycle ++;
+       break;
+     case 1:
+       digitalWrite(beaconLights_LEDpin,LOW);
+       beaconBlinkCycle ++;
+       break;
+     case 2:
+       digitalWrite(beaconLights_LEDpin,LOW);
+       beaconBlinkCycle ++;
+       break;
+     case 3:
+       digitalWrite(beaconLights_LEDpin,LOW);
+       beaconBlinkCycle ++;
+       break;
+     case 4:
+       digitalWrite(beaconLights_LEDpin,LOW);
+       beaconBlinkCycle ++;
+       break;
+     case 5:
+       digitalWrite(beaconLights_LEDpin,HIGH);
+       beaconBlinkCycle ++;
+       break;
+     default:
+       digitalWrite(beaconLights_LEDpin,LOW);
+       beaconBlinkCycle ++;
+       if (beaconBlinkCycle == 10){
+         beaconBlinkCycle = 0;
+       }
+   }
+ }
+}
+TimedAction beaconAction=TimedAction(100,blinkBeacons);
+TimedAction strobeAction=TimedAction(100,blinkStrobes);
+
+
+
+void setup(){
+  for(int a=0;a<14;a++)
+    pinMode(a,OUTPUT);
+  servo1.attach(servo1Pin);
+  servo2.attach(servo2Pin);
+}
+void loop(){
+  beaconAction.check();
+  strobeAction.check();
+  switchFront=(analogRead(tiltFrontAnalogPin)>512);
+  switchBack=(analogRead(tiltBackAnalogPin)>512);
+  switchLeft=(analogRead(tiltLeftAnalogPin)>512);
+  switchRight=(analogRead(tiltRightAnalogPin)>512);
+  analogWrite(motorPin,analogRead(motorDialAnalogPin)/4);
+  button1=digitalRead(button1Pin);
+  button2=digitalRead(button2Pin);
+  button3=digitalRead(button3Pin);
+  button4=digitalRead(button4Pin);
+  if(button1!=button1Last&&button1==true){
+    if(beaconLights_turnedOn==true){
+      beaconLights_turnedOn=false;
+    }else{
+      beaconLights_turnedOn=true;
+    }
+  }
+  button1Last=button1;
+  if(button2!=button2Last&&button2==true){
+    if(strobeLights_turnedOn==true){
+      strobeLights_turnedOn=false;
+    }else{
+      strobeLights_turnedOn=true;
+    }
+  }
+  button2Last=button2;
+  if(button3!=button3Last&&button3==true){
+    if(light3==true){
+      digitalWrite(landingLightsPin,LOW);
+      light3=false;
+    }else{
+      digitalWrite(landingLightsPin,HIGH);
+      light3=true;
+    }
+  }
+  button3Last=button3;
+  if(button4!=button4Last&&button4==true){
+    if(flapsUp==true){
+      //servo1.write(flapsUpAngle);
+      //servo2.write(flapsUpAngle);
+      flapsUp=false;
+    }else{
+      //servo1.write(flapsDownAngle);
+      //servo2.write(flapsDownAngle);
+      flapsUp=true;
+    }
+  }
+  button4Last=button4;
+  if(flapsUp==true){
+    servo1.write(flapsUpAngle);
+    servo2.write(flapsUpAngle);
+  }else{
+    servo1.write(flapsDownAngle);
+    servo2.write(flapsDownAngle);
+  }
+  if(switchFront!=switchFrontLast)
+    timeFront=millis();
+  if ((millis()-timeFront)>100){
+    if(switchFront)
+      digitalWrite(tiltFrontPin,HIGH);
+    else
+      digitalWrite(tiltFrontPin,LOW);
+  }
+  switchFrontLast=switchFront;
+  if(switchBack!=switchBackLast)
+    timeBack=millis();
+  if ((millis()-timeBack)>100){
+    if(switchBack)
+      digitalWrite(tiltBackPin,HIGH);
+    else
+      digitalWrite(tiltBackPin,LOW);
+  }
+  switchBackLast=switchBack;
+  if(switchLeft!=switchLeftLast)
+    timeLeft=millis();
+  if ((millis()-timeLeft)>100){
+    if(switchLeft)
+      digitalWrite(tiltLeftPin,HIGH);
+    else
+      digitalWrite(tiltLeftPin,LOW);
+  }
+  switchLeftLast=switchLeft;
+  if(switchRight!=switchRightLast)
+    timeRight=millis();
+  if ((millis()-timeRight)>100){
+    if(switchRight)
+      digitalWrite(tiltRightPin,HIGH);
+    else
+      digitalWrite(tiltRightPin,LOW);
+  }
+  switchRightLast=switchRight;
+}
